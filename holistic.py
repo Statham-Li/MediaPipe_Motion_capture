74daa5df6863246cf80e1f7cdfad6cc545ae289f
Awen_0x400 <15663589646@163.com>
2023-04-24 16:46:32 +0800

    2023.04.24 feature:fix axis

--- holistic.py
+++ holistic.py
@@ -9,48 +9,157 @@ import argparse
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-from vpython import *
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
+def test():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_holistic = mp.solutions.holistic
 
-points = []
+    fig = plt.figure(figsize=(6, 6))
+    # ax = fig.add_subplot(111, projection="3d")
+    ax = plt.axes(projection='3d')
+    fig.subplots_adjust(left=0.0, right=1, bottom=0, top=1)
+    cap = cv.VideoCapture('./src/squat.mp4')
+    with mp_holistic.Holistic(
+            smooth_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # 加载一个视频的话，把continue换成break
+                break
 
-c = []
-for x in range(33):
-    points.append(sphere(radius=5, pos=vector(0, -50, 0)))
-    c.append(curve(retain=2, radius=4))
-cap = cv.VideoCapture('./src/run.mp4')
-with mp_holistic.Holistic(
-    smooth_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as holistic:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # 加载一个视频的话，把continue换成break
-      break
+            image.flags.writeable = False
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+            results = holistic.process(image)  # 能返回坐标
+            # results = holistic.process(cv.cvtColor(image, cv.COLOR_BGR2RGB)) # 能返回坐标
+            plot_world_landmarks(
+                plt,
+                ax,
+                results.pose_world_landmarks,
+            )
+            # 画图
+            image.flags.writeable = True
+            image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles
+                .get_default_pose_landmarks_style())
 
-    image.flags.writeable = False
-    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-    results = holistic.process(image) # 能返回坐标
-    #画图
-    image.flags.writeable = True
-    image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_holistic.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles
-        .get_default_pose_landmarks_style())
+            # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-    # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            # cv.imshow('MediaPipe Holistic', cv.flip(image, 1))
+            cv.imshow('MediaPipe Holistic', image)
+            if cv.waitKey(5) & 0xFF == 27:
+                break
 
-    cv.imshow('MediaPipe Holistic', cv.flip(image, 1))
-    if cv.waitKey(5) & 0xFF == 27:
-        break
+        cap.release()
+        cv.destroyAllWindows()
+def plot_world_landmarks(
+        plt,
+        ax,
+        landmarks,
+        visibility_th=0.5,
+):
+    landmark_point = []
 
-cap.release()
+    for index, landmark in enumerate(landmarks.landmark):
+        landmark_point.append(
+            [landmark.visibility, (landmark.x, landmark.y, landmark.z)])
+    face_index_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    right_arm_index_list = [11, 13, 15, 17, 19, 21]
+    left_arm_index_list = [12, 14, 16, 18, 20, 22]
+    right_body_side_index_list = [11, 23, 25, 27, 29, 31]
+    left_body_side_index_list = [12, 24, 26, 28, 30, 32]
+    shoulder_index_list = [11, 12]
+    waist_index_list = [23, 24]
+
+    # 面部
+    face_x, face_y, face_z = [], [], []
+    for index in face_index_list:
+        point = landmark_point[index][1]
+        face_x.append(point[0])
+        # face_y.append(point[2])
+        # face_z.append(point[1] * (-1))
+        face_y.append(point[2])
+        face_z.append(point[1] * (-1))
+
+    # 右臂
+    right_arm_x, right_arm_y, right_arm_z = [], [], []
+    for index in right_arm_index_list:
+        point = landmark_point[index][1]
+        right_arm_x.append(point[0])
+        right_arm_y.append(point[2])
+        right_arm_z.append(point[1] * (-1))
+
+    # 左臂
+    left_arm_x, left_arm_y, left_arm_z = [], [], []
+    for index in left_arm_index_list:
+        point = landmark_point[index][1]
+        left_arm_x.append(point[0])
+        left_arm_y.append(point[2])
+        left_arm_z.append(point[1] * (-1))
+
+    # 右半身
+    right_body_side_x, right_body_side_y, right_body_side_z = [], [], []
+    for index in right_body_side_index_list:
+        point = landmark_point[index][1]
+        right_body_side_x.append(point[0])
+        right_body_side_y.append(point[2])
+        right_body_side_z.append(point[1] * (-1))
+
+    # 左半身
+    left_body_side_x, left_body_side_y, left_body_side_z = [], [], []
+    for index in left_body_side_index_list:
+        point = landmark_point[index][1]
+        left_body_side_x.append(point[0])
+        left_body_side_y.append(point[2])
+        left_body_side_z.append(point[1] * (-1))
+
+    # 肩
+    shoulder_x, shoulder_y, shoulder_z = [], [], []
+    for index in shoulder_index_list:
+        point = landmark_point[index][1]
+        shoulder_x.append(point[0])
+        shoulder_y.append(point[2])
+        shoulder_z.append(point[1] * (-1))
+
+    # 腰
+    waist_x, waist_y, waist_z = [], [], []
+    for index in waist_index_list:
+        point = landmark_point[index][1]
+        waist_x.append(point[0])
+        waist_y.append(point[2])
+        waist_z.append(point[1] * (-1))
+
+    ax.cla()
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
+    ax.set_xlim3d(-1, 1)
+    ax.set_ylim3d(-1, 1)
+    ax.set_zlim3d(-1, 1)
+    ax.invert_yaxis()
+    # Mediapipe坐标和matplotlib y z轴相反，且z(mat)需×-1
+    ax.scatter(face_x, face_y, face_z)
+    ax.scatter(0, 0, 0)
+    ax.plot(right_arm_x, right_arm_y, right_arm_z)
+    ax.plot(left_arm_x, left_arm_y, left_arm_z)
+    ax.plot(right_body_side_x, right_body_side_y, right_body_side_z)
+    ax.plot(left_body_side_x, left_body_side_y, left_body_side_z)
+    ax.plot(shoulder_x, shoulder_y, shoulder_z)
+    ax.plot(waist_x, waist_y, waist_z)
+
+    print(shoulder_z[0])
+    plt.pause(.001)
+
+    return
+
+test()
